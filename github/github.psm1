964f2183496efd3aab5434ac1f34d057b08a254d
Yılmaz Can Buhar <7920480+yilmazbuhar@users.noreply.github.com>
2022-01-14 12:52:33 +0300

    fix(github) destination

--- github/github.psm1
+++ github/github.psm1
@@ -2,6 +2,14 @@ Function GitClone([Object[]] $config, [Object[]] $response)
 {
     for ($j=0; $j -lt $response.length; $j++) {
         $fullname = $response[$j].full_name
-        git clone $($config.giturl+"/$fullname")
+        
+        git clone --bare $($config.giturl + "/$fullname")
+        cd $slug".git"
+
+        git remote set-url --push origin $($config.destinationAddress + $slug)
+		git config http.sslVerify false
+		git push --mirror
+
+        cd ..
     }
 }
