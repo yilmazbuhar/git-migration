fe0199ae42787bf5705383ea9a0dda91e5166b96
Yılmaz Can Buhar <7920480+yilmazbuhar@users.noreply.github.com>
2022-01-14 12:02:07 +0300

    refactor(migrate)

--- bitbucket/bitbucket.psm1
+++ bitbucket/bitbucket.psm1
@@ -3,7 +3,21 @@ Function GitClone([Object[]] $config, [Object[]] $response)
     for ($j=0; $j -lt $response.values.Length; $j++) {
 		
         $fullname = $response.values[$j].full_name
+        $slug = $response.values[$j].slug
+
+	git clone --bare "$($config.giturl+"/$fullname")"
+
+        cd $slug".git"
+
+	git remote set-url --push origin $($config.destinationAddress + "/"+ $slug)
+	git config http.sslVerify false
+	git push --mirror
         
-		git clone "$($config.giturl+"/$fullname")"
+        cd ..
+		
+	#$location = Get-Location
+	#$rmfolder = ("$location" + "\$slug").toString()
+	#$rmfolder
+	#Remove-Item $rmfolder -recurse -force
     }
 }
